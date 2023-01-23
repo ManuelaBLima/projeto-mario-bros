5fbd7c5583e14dfdc802f6f4e71a54be1cd53f65
ManuelaBLima <123252735+ManuelaBLima@users.noreply.github.com>
2023-01-23 19:27:43 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+/* console.log('mostrar o document',document);
+
+console.log(document.querySelector(".botao-trailer"));
+ */
+
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
+const botaoFecharModal= document.querySelector(".fechar-modal");
+const linkDoVideo = video.scr;
+
+
+function alternaModal () {
+    modal.classList.toggle("aberto") ; 
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternaModal();
+video.setAttribute("src", linkDoVideo);
+})
+
+botaoFecharModal.addEventListener("click", () => {
+    alternaModal();
+    video.setAttribute("src","");
+});
+
